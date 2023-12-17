59245acec72896ab8435e5e654164b49feb0d89a
YuriiRogovets <yura.rogovets@gmail.com>
2023-12-17 17:40:16 +0200

    update task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,5 +1,12 @@
 const nameInput = document.querySelector("#name-input")
 const nameOutput = document.querySelector("#name-output")
+// nameInput.addEventListener("input", (event) => {
+//   nameOutput.textContent = event.currentTarget.value;
+// });
+
 nameInput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.currentTarget.value;
+  nameOutput.textContent = event.currentTarget.value.trim();
+  if (nameOutput.textContent === ``) {
+    nameOutput.textContent = `Anonymous`;
+  }
 });
