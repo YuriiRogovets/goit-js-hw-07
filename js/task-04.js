b57a102bb4bd94bbca75224e208a3576f63192ae
YuriiRogovets <yura.rogovets@gmail.com>
2023-12-18 01:27:21 +0200

    update css and comments mentor

--- js/task-04.js
+++ js/task-04.js
@@ -5,13 +5,14 @@ loginForm.addEventListener("submit", handleForm);
 const obj = {};
 function handleForm(event) {
     event.preventDefault();
-    const eml = loginForm.elements.email.value;
-    const psw = loginForm.elements.password.value;
-	if (eml === "" || psw === "") {
-        alert('All form fields must be filled in');
+    const emailEl = loginForm.elements.email.value;
+    const psswordEl = loginForm.elements.password.value;
+	if (emailEl === "" || psswordEl === "") {
+       return alert ('All form fields must be filled in');
     }
-    obj.email = eml.trim();
-    obj.password = psw.trim();
+
+    obj.email = emailEl.trim();
+    obj.password = psswordEl.trim();
     console.log(obj);
     loginForm.reset();
 };
